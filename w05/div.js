443b5b7401e18d8332a10fa5eb59953624f85eee
Stuvz <Terryluigi999@Gmail.com>
2021-04-15 23:53:08 +0800

    submitted w08 homeworks

--- w05/div.js
+++ w05/div.js
@@ -25,13 +25,36 @@ function changeColor(i) {
   console.log('section', p);
   switch (i) {
     case 1:
-     p.style.backgroundColor = 'yellow';
+      p.style.backgroundColor = 'yellow';
       break;
     case 2:
-        p.style.backgroundColor = 'white';
+      p.style.backgroundColor = 'white';
       break;
     case 3:
-        p.style.backgroundColor = 'black';
+      p.style.backgroundColor = 'black';
       break;
   }
+}
+
+function changeImg(i) {
+  const p = document.querySelector("#player");
+  console.log("player", p);
+  switch (i) {
+    case 1:
+      p.innerHTML = `<img src="img1.jpg" style="width: 100%; height: auto; position:absolute; top: 0; bottom: 0; left: 0; right: 0; margin:auto" />`;
+      break;      
+    case 2:
+      p.innerHTML = `<img src="img2.jpg" style="width: 100%; height: auto; position:absolute; top: 0; bottom: 0; left: 0; right: 0; margin:auto" />`;
+      break;
+    case 3:
+      p.innerHTML = `<img src="img3.jpg" style="width: 100%; height: auto; position:absolute; top: 0; bottom: 0; left: 0; right: 0; margin:auto" />`;
+      break;
+    case 4:
+      p.innerHTML = `<img src="img4.jpg" style="width: 100%; height: auto; position:absolute; top: 0; bottom: 0; left: 0; right: 0; margin:auto" />`;
+      break;
+    case 5:
+      p.innerHTML = `<img src="img5.jpg" style="width: 100%; height: auto; position:absolute; top: 0; bottom: 0; left: 0; right: 0; margin:auto" />`;
+      break;
+  }
+
 }
